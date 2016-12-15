698f51604d3ef2b4f4b466b5b933e98d9c09ba91
aloy42 <denis.semendyayev.508@my.csun.edu>
2016-12-14 17:45:45 -0800

    Add files via upload

--- terraform/var.tf
+++ terraform/var.tf
@@ -24,7 +24,3 @@ variable "vpc_cidr" {
   description = "VPC CIDR for usage throughout"
   default = "172.31.0.0/16"
 }
-variable "vpc_id" {
-  description = "VPC ID for usage throughout the build process"
-  default = "vpc-f1876e96"
-}
